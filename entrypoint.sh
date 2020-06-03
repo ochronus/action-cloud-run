6fb810a3cb4ec0e5ec20c13b00d5cf30a0375808
Csaba Okrona <ochronus@ochronus.com>
2020-06-03 14:38:26 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,19 @@ echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/gcloud.json
 
 if [ "$INPUT_ENVVARS" ]
 then
-    ENV_FLAG="--set-env-vars $INPUT_ENVVARS"
+    ENV_FLAG="--set-env-vars '$INPUT_ENVVARS'"
 else
     ENV_FLAG=""
 fi
+echo "ENV_FLAG"
+echo $ENV_FLAG
 
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
 gcloud auth configure-docker
 
 docker push "$INPUT_IMAGE"
 
-gcloud beta run deploy "$INPUT_SERVICE" \
+gcloud run deploy "$INPUT_SERVICE" \
   --image "$INPUT_IMAGE" \
   --region "$INPUT_REGION" \
   --platform managed \
